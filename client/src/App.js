79866a227639604ac24b473a03499fd2ac2ac67c
Jess Daniel <jessdaniel474@gmail.com>
2019-11-15 11:30:46 -0500

    Login form added to get token from server

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           display BubblePage when you're authenticated 
         */}
       </div>
+      <Route exact path="/bubbles-page" component={BubblePage} />
     </Router>
   );
 }
